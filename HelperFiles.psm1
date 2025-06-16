0a539fc21d1124f1dc9c06e944f60eb05cb7811f
Ilya Kochetkov <ie.kochetkov@yandex.ru>
2025-06-16 20:39:36 +0300

    Оптимизация записи XML ролей

--- HelperFiles.psm1
+++ HelperFiles.psm1
@@ -34,49 +34,73 @@ function Remove-Backups {
     }
 }
 
+
 function Remove-ParasiteRights {
-	param (
-		[string]$roleFileFullName
-	)
-	
-	[xml]$xml = Get-Content -Path $roleFileFullName
+    param (
+        [string]$roleFileFullName
+    )
+  
+    # Считаем исходный файл как текст
+    $fileText = Get-Content -Raw -Encoding UTF8 $roleFileFullName
 
-	# Создаем менеджер пространств имен и регистрируем префикс
-	$nsMgr = New-Object System.Xml.XmlNamespaceManager($xml.NameTable)
-	$nsMgr.AddNamespace("r", "http://v8.1c.ru/8.2/roles")
+    # Определяем перенос строк в исходном файле (по первому совпадению)
+    if ($fileText -match "`r`n") {
+        $newLineChars = "`r`n"  # Windows style CRLF
+    } elseif ($fileText -match "`r") {
+        $newLineChars = "`r"    # Mac old style CR
+    } elseif ($fileText -match "`n") {
+        $newLineChars = "`n"    # Unix style LF
+    } else {
+        # По умолчанию ставим CRLF (можно и LF)
+        $newLineChars = "`r`n"
+    }
 
-	# Удаляем все узлы <right>, у которых <value> равно "false"
-	$rightNodes = $xml.SelectNodes("//r:object/r:right[r:value='false']", $nsMgr)
-	foreach ($right in $rightNodes) {
-		$right.ParentNode.RemoveChild($right) | Out-Null
-	}
+    [xml]$xml = $fileText
 
-	# Удаляем все узлы <object>, у которых не осталось узлов <right>
-	$objectNodes = $xml.SelectNodes("//r:object[not(r:right)]", $nsMgr)
-	foreach ($object in $objectNodes) {
-		$object.ParentNode.RemoveChild($object) | Out-Null
-	}
+    # Создаем менеджер пространств имен и регистрируем префикс
+    $nsMgr = New-Object System.Xml.XmlNamespaceManager($xml.NameTable)
+    $nsMgr.AddNamespace("r", "http://v8.1c.ru/8.2/roles")
 
-	# Сохраняем результат
-	$xml.Save($roleFileFullName)
+    # Удаляем все узлы <right>, у которых <value> равно "false"
+    $rightNodes = $xml.SelectNodes("//r:object/r:right[r:value='false']", $nsMgr)
+    foreach ($right in $rightNodes) {
+        $right.ParentNode.RemoveChild($right) | Out-Null
+    }
 
+    # Удаляем все узлы <object>, у которых не осталось узлов <right>
+    $objectNodes = $xml.SelectNodes("//r:object[not(r:right)]", $nsMgr)
+    foreach ($object in $objectNodes) {
+        $object.ParentNode.RemoveChild($object) | Out-Null
+    }
+
+    # Настройки XmlWriter с переносами строк из исходного файла
+    $settings = New-Object System.Xml.XmlWriterSettings
+    $settings.Indent = $true
+    $settings.IndentChars = "`t"
+    $settings.NewLineChars = $newLineChars
+    $settings.NewLineHandling = "Replace"
+
+    $writer = [System.Xml.XmlWriter]::Create($roleFileFullName, $settings)
+    $xml.Save($writer)
+    $writer.Close()
 }
 
+
 function Optimize-RoleSizes {
-	param (
-		[string]$sourcePath
-	)
+    param (
+        [string]$sourcePath
+    )
 	
-	# Ищем папки с именем Roles рекурсивно
-	$rolesDirs = Get-ChildItem -Path $sourcePath -Directory -Recurse | Where-Object { $_.Name -eq 'Roles' }
+    # Ищем папки с именем Roles рекурсивно
+    $rolesDirs = Get-ChildItem -Path $sourcePath -Directory -Recurse | Where-Object { $_.Name -eq 'Roles' }
 
-	foreach ($dir in $rolesDirs) {
-		# Ищем нужные файлы в каталоге Roles и его подкаталогах
-  		$files = Get-ChildItem -Path $dir.FullName -Recurse -File | Where-Object { $_.Name -in @('Rights.rights', 'Rights.xml') }
+    foreach ($dir in $rolesDirs) {
+        # Ищем нужные файлы в каталоге Roles и его подкаталогах
+        $files = Get-ChildItem -Path $dir.FullName -Recurse -File | Where-Object { $_.Name -in @('Rights.rights', 'Rights.xml') }
 
-  		foreach ($file in $files) {
-			Remove-ParasiteRights -roleFileFullName $file.FullName
-  		}
-	}
+        foreach ($file in $files) {
+            Remove-ParasiteRights -roleFileFullName $file.FullName
+        }
+    }
 
 }
