d374467a0de9e371b0c5b3cc6f335198fa646dc6
Ilya Kochetkov <ikochetkov@rn-leasing.ru>
2025-08-27 16:28:05 +0300

    Обертка для выгрузки релиза

--- HelperReleases.psm1
+++ HelperReleases.psm1
@@ -17,3 +17,44 @@ function Get-LatestReleasePath {
 		
 	return $($latestFile.FullName)
 }
+
+
+function Publish-GenericPackageToGLRegistry {
+	param (
+		[pscustomobject[]]$mainConfig,
+		[string]$gLServer,
+		[string]$gLUser,
+		[string]$prName,
+		[string]$FilePath,
+		[string]$PackageName,
+		[string]$PackageVersion
+	)
+
+
+	# Получаем конфиг сервера GitLab/Registry
+	$serverConfig = Get-ServerConfig -mainConfig $mainConfig -server $gLServer
+
+	$login = Get-LoginFromConfig -serverConfig $serverConfig -loginType "registry"
+	$token = [string]$login.password
+
+	# Базовый URL для обращений к GitLab API
+	$baseUrl = $gLServer.TrimEnd('/')
+
+	# Находим описание проекта в databases по имени
+	$projectConfig = Get-DataBaseConfig -serverConfig $serverConfig -baseName $prName
+
+	# Определяем идентификатор проекта из свойства 'internal' записи databases
+	$projectId = [string]$projectConfig.internal
+
+	# URL загрузки
+	$fileName = [System.IO.Path]::GetFileName($FilePath)
+	$uri = "$baseUrl/api/v4/projects/$projectId/packages/generic/$PackageName/$PackageVersion/1cv8.cf"
+
+	# Выполняем загрузку через Native PowerShell (Invoke-WebRequest PUT)
+	$headers = @{ 'PRIVATE-TOKEN' = $token }
+	Write-Host "Uploading '$fileName' to registry: $uri" -ForegroundColor Yellow
+	$response = Invoke-WebRequest `
+		-Uri $uri -Method Put -Headers $headers `
+		-InFile $FilePath -ContentType 'application/octet-stream' -UseBasicParsing
+	return $response
+}
